06bbd11120806bb55278b6dfc2b51f035497a8f9
PopovaElis <elizprest@gmail.com>
2022-08-26 17:51:06 +0500

    Готовое дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // task 1
+        {  String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName= "Ivanov";
+        firstName= lastName+ " "+firstName+ " "+middleName;
+        System.out.println("ФИО сотрудника — "+firstName);
+
+        //task 2
+        String firstNameUp= firstName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — "+firstNameUp);
     }
-}
+        //task 3
+        String fullName= "Иванов Семён Семёнович";
+        System.out.println("ФИО сотрудника — "+ fullName.replace('ё', 'е'));
+    }
+        }
